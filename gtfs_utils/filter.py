Start creating new filter cli and fix problems with non-lazy computation for filter function

gtfs_utils/filter.py
@@ -1,17 +1,20 @@
 import logging
 import time
 from pathlib import Path
+from typing import List
 
 import numpy as np
 import shapely
 import geopandas as gpd
+
+from gtfs_utils.utils import GtfsDict, compute_if_necessary
 
 
 def cleanup_stop_transfers(df_dict, output_dir: Path, all_stop_ids):
     t = time.time()
     mask = df_dict["stops"]["stop_id"].isin(all_stop_ids)
     df_dict["stops"] = df_dict["stops"][mask]
-    df_dict["stops"].to_csv(output_dir / "stops.txt", single_file=True, index=False)
+    df_dict.save_file("stops", output_dir)
     duration = time.time() - t
     logging.debug(f"Filtered stops.txt for {duration:.2f}s")
 
@@ -22,25 +25,22 @@ def cleanup_stop_transfers(df_dict, output_dir: Path, all_stop_ids):
             "transfers"
         ]["to_stop_id"].isin(all_stop_ids)
         df_dict["transfers"] = df_dict["transfers"][mask]
-        df_dict["transfers"].to_csv(
-            output_dir / "transfers.txt", single_file=True, index=False
-        )
+        df_dict.save_file("transfers", output_dir)
         duration = time.time() - t
         logging.debug(f"Filtered transfers.txt for {duration:.2f}s")
 
 
-def cleanup_calendar(df_dict, output_dir: Path):
+def cleanup_calendar(df_dict: GtfsDict, output_dir: Path):
     if "calendar" in df_dict or "calendar_dates" in df_dict:
-        service_ids = df_dict["trips"]["service_id"].unique().compute()
+        service_ids = compute_if_necessary(df_dict["trips"]["service_id"].unique())
 
         # filter calendar
         if "calendar" in df_dict:
             t = time.time()
             mask = df_dict["calendar"]["service_id"].isin(service_ids)
             df_dict["calendar"] = df_dict["calendar"][mask]
-            df_dict["calendar"].to_csv(
-                output_dir / "calendar.txt", single_file=True, index=False
-            )
+            df_dict.save_file("calendar", output_dir)
+
             duration = time.time() - t
             logging.debug(f"Filtered calendar.txt for {duration:.2f}s")
 
@@ -49,9 +49,8 @@ def cleanup_calendar(df_dict, output_dir: Path):
             t = time.time()
             mask = df_dict["calendar_dates"]["service_id"].isin(service_ids)
             df_dict["calendar_dates"] = df_dict["calendar_dates"][mask]
-            df_dict["calendar_dates"].to_csv(
-                output_dir / "calendar_dates.txt", single_file=True, index=False
-            )
+            df_dict.save_file("calendar_dates", output_dir)
+
             duration = time.time() - t
             logging.debug(f"Filtered calendar_dates.txt for {duration:.2f}s")
 
@@ -86,7 +85,13 @@ def remove_route_with_type(df_dict, output, types):
     cleanup_calendar(df_dict, output_dir)
 
 
-def filter_gtfs(df_dict, filter_geometry, output, shapes=False, complete_trips=False):
+def filter_gtfs(
+    df_dict: GtfsDict,
+    filter_geometry: List[float] | shapely.geometry.base.BaseGeometry,
+    output: str | Path,
+    shapes=False,
+    complete_trips=False,
+):
     output_dir = Path(output)
 
     if isinstance(filter_geometry, list):
@@ -96,7 +101,7 @@ def filter_gtfs(df_dict, filter_geometry, output, shapes=False, complete_trips=F
     else:
         raise ValueError(f"filter_geometry type {type(filter_geometry)} not supported!")
 
-    dic = df_dict["stops"][["stop_lon", "stop_lat", "stop_id"]].compute()
+    dic = compute_if_necessary(df_dict["stops"][["stop_lon", "stop_lat", "stop_id"]])
 
     gpd_data = gpd.GeoDataFrame(
         dic["stop_id"], geometry=gpd.points_from_xy(dic.stop_lon, dic.stop_lat)
@@ -115,16 +120,14 @@ def filter_gtfs(df_dict, filter_geometry, output, shapes=False, complete_trips=F
     mask = df_dict["stop_times"]["stop_id"].isin(stop_ids)
     del stop_ids
     unique_trip_ids = df_dict["stop_times"][mask]["trip_id"].unique()
-    trip_ids = unique_trip_ids.compute()
+    trip_ids = compute_if_necessary(unique_trip_ids)
 
     if complete_trips:
         mask = df_dict["stop_times"]["trip_id"].isin(trip_ids)
 
-    all_stop_ids = df_dict["stop_times"][mask]["stop_id"].unique().compute()
+    all_stop_ids = compute_if_necessary(df_dict["stop_times"][mask]["stop_id"])
     df_dict["stop_times"] = df_dict["stop_times"][mask]
-    df_dict["stop_times"].to_csv(
-        output_dir / "stop_times.txt", single_file=True, index=False
-    )
+    df_dict.save_file("stop_times", output_dir)
     duration = time.time() - t
     logging.debug(f"Filtered stop_times.txt for {duration:.2f}s")
 
@@ -134,7 +137,7 @@ def filter_gtfs(df_dict, filter_geometry, output, shapes=False, complete_trips=F
     df_dict["trips"] = df_dict["trips"][mask]
     if not shapes:
         df_dict["trips"] = df_dict["trips"].assign(shape_id=np.nan)
-    df_dict["trips"].to_csv(output_dir / "trips.txt", single_file=True, index=False)
+    df_dict.save_file("trips", output_dir)
     del trip_ids
     duration = time.time() - t
     logging.debug(f"Filtered trips.txt for {duration:.2f}s")
@@ -151,17 +154,17 @@ def filter_gtfs(df_dict, filter_geometry, output, shapes=False, complete_trips=F
     # Filter route.txt
     t = time.time()
     route_ids = df_dict["trips"]["route_id"].unique()
-    mask = df_dict["routes"]["route_id"].isin(route_ids.compute())
+    mask = df_dict["routes"]["route_id"].isin(compute_if_necessary(route_ids))
     df_dict["routes"] = df_dict["routes"][mask]
-    df_dict["routes"].to_csv(output_dir / "routes.txt", single_file=True, index=False)
+    df_dict.save_file("routes", output_dir)
     duration = time.time() - t
     logging.debug(f"Filtered routes.txt for {duration:.2f}s")
 
     # Filter agency.txt
     t = time.time()
     agency_ids = df_dict["routes"]["agency_id"].unique()
-    mask = df_dict["agency"]["agency_id"].isin(agency_ids.compute())
+    mask = df_dict["agency"]["agency_id"].isin(compute_if_necessary(agency_ids))
     df_dict["agency"] = df_dict["agency"][mask]
-    df_dict["agency"].to_csv(output_dir / "agency.txt", single_file=True, index=False)
+    df_dict.save_file("agency", output_dir)
     duration = time.time() - t
     logging.debug(f"Filtered agency.txt for {duration:.2f}s")